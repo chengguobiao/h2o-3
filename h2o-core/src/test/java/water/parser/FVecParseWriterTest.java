528eb860d1555c2d9657d02ac3a5abab306d3e4a
michalkurka <michalk@h2o.ai>
2018-08-17 10:09:19 -0700

    Demonstrates the Parser produces same results as before

--- h2o-core/src/test/java/water/parser/FVecParseWriterTest.java
+++ h2o-core/src/test/java/water/parser/FVecParseWriterTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 import water.Key;
 import water.fvec.AppendableVec;
 import water.fvec.Chunk;
+import water.fvec.NewChunk;
 import water.fvec.Vec;
+
+import java.util.Random;
 
 public class FVecParseWriterTest {
 
@@ -18,11 +21,39 @@ public class FVecParseWriterTest {
   }
 
   @Test
-  public void addNumColLosesPrecision() {
+  public void addNumColLosesPrecision() { // needs to be fixed: PUBDEV-5840
     writer.addNumCol(0, Math.PI);
     double h2oPI = writer._nvs[0].compress().atd(0);
     Assert.assertEquals(h2oPI, Math.PI, 1e-15);
     Assert.assertNotEquals(h2oPI, Math.PI, 0);
+  }
+
+  @Test
+  public void testBackwardsCompatibility() {
+    Random r = new Random(42);
+    FVecParseWriter wr2 = makeWriter();
+    for (int i = 0; i < 2000; i++) {
+      double v = r.nextDouble();
+      wr2.addNumCol(0, v);
+      oldAddNumDecompose(v, 0, writer);
+    }
+    Chunk nc = writer._nvs[0].compress();
+    Chunk nc2 = wr2._nvs[0].compress();
+    for (int i = 0; i < nc._len; i++) {
+      Assert.assertEquals(nc2.atd(i), nc.atd(i), 0);
+    }
+  }
+
+  private static void oldAddNumDecompose(final double value, int colIdx, FVecParseWriter w) {
+    double d = value;
+    int exp = 0;
+    long number = (long)d;
+    while (number != d) {
+      d *= 10;
+      --exp;
+      number = (long) d;
+    }
+    w.addNumCol(colIdx, number, exp);
   }
 
   @Test(timeout = 10000)
